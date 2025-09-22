cb4b6af8707fcfbf5c3fa9bf45f349ba0f92db94
Mohammed Rafeeq <124717010+Mohammed-Rafeeq13@users.noreply.github.com>
2025-09-22 19:19:21 +0530

    Completed rotate_image.cpp

--- 2-D_Array/rotate_image.cpp
+++ 2-D_Array/rotate_image.cpp
@@ -1,28 +1,43 @@
 #include<iostream>
 using namespace std;
 int main(){
- int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
- for(int i=0;i<3;i++){
-  for(int j=i;j<3;j++){
-   int temp = arr[i][j];
-   arr[i][j] = arr[j][i];
-   arr[j][i] = temp;
-  }
- }
- for(int i=0;i<3;i++){
-  for(int j=0;j<3/2;j++){
-   int temp = arr[i][j];
-    arr[i][j] = arr[i][3-1-j];
-    arr[i][3-1-j] = temp;
-   }
- }
-
- cout<<"Transpose Matrix : "<<endl;
- for(int i=0;i<3;i++){
-  for(int j=0;j<3;j++){
-   cout<<arr[i][j]<<" ";
-  }
-  cout<<endl;
- }
- return 0;
+    int n,m;
+    cout<<"Enter number of rows : ";
+    cin>>n;
+    cout<<"Enter number of columns : ";
+    cin>>m;
+    int arr[n][m];
+    cout<<"Enter the Elements in the Array : "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"Matrix is : "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i;j<m;j++){
+            swap(arr[i][j],arr[j][i]);
+        }
+    }
+    cout<<"Transpose Matrix : "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"Rotate Image Matrix : "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=m-1;j>=0;j--){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
